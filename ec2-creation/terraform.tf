42e4b238b9a1b80cb7ed453bff6c807c39ad3a0c
Ruchali Sudke <111962928+Ruchesha@users.noreply.github.com>
2024-09-11 19:27:04 +0200

    Update terraform.tf

--- ec2-creation/terraform.tf
+++ ec2-creation/terraform.tf
@@ -5,4 +5,10 @@ terraform {
       version = "5.65.0"
     }
   }
+ backend "s3" {
+	bucket = "demo-bucket-for-remote-backends"
+	key = "terraform.tfstate"
+	region = "us-east-2"
+	dynamodb_table = "demo-dynamodb-table"
+ }
 }
