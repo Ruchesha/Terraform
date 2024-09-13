de3eda57a16ec8ae43915a9e0dfde2ddf9788d9d
Ruchali Sudke <111962928+Ruchesha@users.noreply.github.com>
2024-09-13 15:53:07 +0200

    Create main.tf

---
+++ Infrastructure/main.tf
@@ -0,0 +1,26 @@
+module "dev-infra" {
+	      source = "./my_infra"
+	      my_env = "dev"
+	      ec2_count = 1
+	      dynamo_table_name = "dev-infra-table"
+	      ec2_instance_type = "t2.micro"
+	      ec2_ami_id = "ami-0182f373e66f89c85"
+}
+
+module "staging-infra" {
+        source = "./my_infra"
+        my_env = "stg"
+        ec2_count = 1
+        dynamo_table_name = "stg-infra-table"
+        ec2_instance_type = "t2.micro"
+        ec2_ami_id = "ami-0182f373e66f89c85"
+}
+
+module "prod-infra" {
+        source = "./my_infra"
+        my_env = "prod"
+        ec2_count = 1
+        dynamo_table_name = "prod-infra-table"
+        ec2_instance_type = "t2.micro"
+        ec2_ami_id = "ami-0182f373e66f89c85"
+}
