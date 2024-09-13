32586244a31a00f93a98d9dac8cb42e32954b83a
Ruchali Sudke <111962928+Ruchesha@users.noreply.github.com>
2024-09-13 16:02:58 +0200

    Delete s3.tf

--- s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "my_bucket" {
-
-        bucket = "practice-bucket-9422"
-        tags = {
-                Name = "demo bucket"
-                Environment = "dev"
-        }
-}
